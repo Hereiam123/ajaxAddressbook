ea196f4c82731bd58bbbe5b59044af417191232f
hereiam123 <bdemaio1@gmail.com>
2017-06-09 18:24:12 -0400

    added a gif during loading from contacts.php, using jquery

--- index.php
+++ index.php
@@ -147,81 +147,21 @@
             </div>
         </div>
     </div>
-    <div class="row">
-        <div class="large-12 columns">
-            <table>
-                <thead>
-                <tr>
-                    <th width="200">Name</th>
-                    <th width="130">Phone</th>
-                    <th width="200">Email</th>
-                    <th width="250">Address</th>
-                    <th width="100">Group</th>
-                    <th width="150">Actions</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td><a href="contact.html">John Doe</a></td>
-                    <td>978 388-1234</td>
-                    <td>jdoe@gmail.com</td>
-                    <td>
-                        <ul>
-                            <li>55 Main Street</li>
-                            <li>Amesbury, MA 01913</li>
-                        </ul>
-                    </td>
-                    <td>Family</td>
-                    <td>
-                        <ul class="button-group expanded">
-                            <li><a href="#" class="button tiny">Edit</a></li>
-                            <li><a href="#" class="button tiny alert">Delete</a></li>
-                        </ul>
-                    </td>
-                </tr>
-                <tr>
-                    <td><a href="contact.html">John Doe</a></td>
-                    <td>978 388-1234</td>
-                    <td>jdoe@gmail.com</td>
-                    <td>
-                        <ul>
-                            <li>55 Main Street</li>
-                            <li>Amesbury, MA 01913</li>
-                        </ul>
-                    </td>
-                    <td>Family</td>
-                    <td>
-                        <ul class="button-group expanded">
-                            <li><a href="#" class="button tiny">Edit</a></li>
-                            <li><a href="#" class="button tiny alert">Delete</a></li>
-                        </ul>
-                    </td>
-                </tr>
-                <tr>
-                    <td><a href="contact.html">John Doe</a></td>
-                    <td>978 388-1234</td>
-                    <td>jdoe@gmail.com</td>
-                    <td>
-                        <ul>
-                            <li>55 Main Street</li>
-                            <li>Amesbury, MA 01913</li>
-                        </ul>
-                    </td>
-                    <td>Family</td>
-                    <td>
-                        <ul class="button-group expanded">
-                            <li><a href="#" class="button tiny">Edit</a></li>
-                            <li><a href="#" class="button tiny alert">Delete</a></li>
-                        </ul>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
+
+    <!--Loading Image-->
+    <div id="loaderImage">
+        <img src="img/ajax-loader.gif">
+    </div>
+
+    <!--Main Content-->
+    <div id="pageContent">
+
+
     </div>
 
 
     <script src="js/vendor/jquery.js"></script>
+    <script src="js/script.js"></script>
     <script src="js/vendor/what-input.js"></script>
     <script src="js/vendor/foundation.js"></script>
     <script src="js/app.js"></script>
