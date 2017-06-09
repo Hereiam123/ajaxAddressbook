b6b39f91ba68e9fcb1b69a790368e8bb1912dc15
hereiam123 <bdemaio1@gmail.com>
2017-06-08 20:14:38 -0400

    setup init file to load required files

---
+++ core/init.php
@@ -0,0 +1,14 @@
+<?php
+
+//Include config
+require_once('config/config.php');
+
+//Helper files
+require_once('helpers/system_helper.php');
+
+//autoloader
+function spl_autoload_register($class_name){
+    require_once('libraries/'.$class_name.'php');
+}
+
+
