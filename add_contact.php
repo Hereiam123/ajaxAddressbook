2cfe3839853af06c9b3c9d6233dad0f3241cc653
hereiam123 <bdemaio1@gmail.com>
2017-06-09 19:21:33 -0400

    forgot to use prepared statements in add_contact, reconfigured to prevent problems

--- add_contact.php
+++ add_contact.php
@@ -6,26 +6,30 @@ $db=new Database;
 
 if(isset($_POST))
 {
-    $first_name=$_POST['first_name'];
-    $last_name=$_POST['last_name'];
-    $email=$_POST['email'];
-    $phone=$_POST['phone'];
-    $contact_group=$_POST['contact_group'];
-    $address1=$_POST['address1'];
-    $address2=$_POST['address2'];
-    $city=$_POST['city'];
-    $state=$_POST['state'];
-    $zipcode=$_POST['zipcode'];
-    $notes=$_POST['notes'];
-
     //Run Query
     $db->query("INSERT INTO `contacts` (first_name,last_name,email,
                 phone,contact_group,address1,address2,city,state,zipcode,notes)
-                VALUES ('$first_name','$last_name','$email','$phone','$contact_group',
-                '$address1','$address2','$city','$state','$zipcode','$notes')");
+                VALUES (:first_name,:last_name,:email,:phone,:contact_group,
+                :address1,:address2,:city,:state,:zipcode,:notes)");
 
-    //Assign results
-    $contacts=$db->resultset();
+    $db->bind(':first_name',$_POST['first_name']);
+    $db->bind(':last_name',$_POST['last_name']);
+    $db->bind(':email',$_POST['email']);
+    $db->bind(':phone',$_POST['phone']);
+    $db->bind(':address1',$_POST['address1']);
+    $db->bind(':address2',$_POST['address2']);
+    $db->bind(':city',$_POST['city']);
+    $db->bind(':state',$_POST['state']);
+    $db->bind(':zipcode',$_POST['zipcode']);
+    $db->bind(':contact_group',$_POST['contact_group']);
+    $db->bind(':notes',$_POST['notes']);
+
+    if($db->execute()){
+        echo "Contact was added";
+    }
+    else{
+        echo "Contact not added";
+    }
 }
 
 ?>
